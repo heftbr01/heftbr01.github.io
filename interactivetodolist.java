69a2f51c3c438e48818b601e6f051c4409fff476
heftbr01 <heftbr01@luther.edu>
2015-10-18 19:25:35 -0500

    Update interactivetodolist.java

--- interactivetodolist.java
+++ interactivetodolist.java
@@ -1,1 +1,7 @@
-gdnd
+makePage = function() {
+  paragraph = document.createElement("p");
+  pcontents = document.createTextNode("This is a paragraph followed by a list");
+  
+  paragraph.appendChild(pcontents);
+  document.body.appendChild(paragraph);
+}
