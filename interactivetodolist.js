d784ca90746bbe756b715e2d7fb58621681e6d92
heftbr01 <heftbr01@luther.edu>
2015-10-19 14:26:51 -0500

    Create interactivetodolist.js

---
+++ interactivetodolist.js
@@ -0,0 +1,7 @@
+makePage = function() {
+  lcontents1 = document.createElement("li");
+  lcontents = document.createTextNode("This is a paragraph followed by a list");
+  
+  lcontents1.appendChild(lcontents);
+  document.body.section.ul.appendChild(lcontents1);
+}
